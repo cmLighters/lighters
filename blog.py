2d5c9613b5b102e3a03098a6c85022231bd916f2
cmLighters <mijechen@yeah.net>
2018-03-04 22:54:19 +0800

    add custome error pages

--- blog.py
+++ blog.py
@@ -12,3 +12,10 @@ def index():
 def user(username):
     return render_template('user.html', username=username)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
