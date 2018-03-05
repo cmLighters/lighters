644a5710359492965b03a2f8d9bebc4ef85fab5b
cmLighters <mijechen@yeah.net>
2018-03-05 10:45:30 +0800

    use msyql database

--- blog.py
+++ blog.py
@@ -1,5 +1,7 @@
 #from datetime import datetime
-from flask import Flask, render_template, redirect, url_for, session
+import os
+from flask import Flask, render_template, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -9,23 +11,57 @@ from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Can u guess it.'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{username}:{password}@{hostname}/{database}'.format(
+    username='testuser', password='asdfjkl;', hostname='127.0.0.1', database='cm_flask_blog'
+)
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     #name = None
     form = NameForm()
     if form.validate_on_submit():
-        session['name'] = form.name.data
+        name = form.name.data
+        user = User.query.filter_by(username=name).first()
+        if user is None:
+            user = User(username=name)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = name
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', name=session.get('name'), form=form)
+    return render_template('index.html', name=session.get('name'), known=session.get('known', False), form=form)
 
 @app.route('/user/<username>')
 def user(username):
