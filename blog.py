bbd21c9af015394c534329e278e4c17ba21617f2
cmLighters <mijechen@yeah.net>
2018-03-05 17:55:12 +0800

    change directory to big project structure

--- blog.py
+++ blog.py
@@ -1,53 +1,10 @@
-#from datetime import datetime
 import os
-from threading import Thread
-from flask import Flask, render_template, redirect, url_for, session, flash
-from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
-from flask_moment import Moment
-from flask_mail import Mail, Message
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from app import create_app, db
+from app.models import Role, User
 
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-# database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{username}:{password}@{hostname}/{database}'.format(
-    username='testuser', password='asdfjkl;', hostname='127.0.0.1', database='cm_flask_blog'
-)
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-# mail
-app.config['MAIL_SERVER'] = 'smtp.yeah.net'
-app.config['MAIL_PORT'] = 25
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_SUBJECT_PREFFIX'] = '[cmLighters\' Blog]'
-app.config['MAIL_SENDER'] = 'Blog Admin <mijechen@yeah.net>'
-app.config['MAIL_ADMIN'] = os.environ.get('MAIL_ADMIN')
-
-
-def send_email(to, subject, template, **kwargs):
-    msg = Message(app.config['MAIL_SUBJECT_PREFFIX']+subject, sender=app.config['MAIL_SENDER'], recipients=[to])
-    msg.body = render_template(template+'.txt', **kwargs)
-    msg.html = render_template(template+'.html', **kwargs)
-    thr = Thread(target=send_email_async, args=[msg])
-    thr.start()
-    return thr
-    #mail.send(msg)
-
-def send_email_async(msg):
-    with app.app_context():
-        mail.send(msg)
-
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-db = SQLAlchemy(app)
+app = create_app(os.environ.get('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
-mail = Mail(app)
 
 
 @app.shell_context_processor
@@ -55,59 +12,6 @@ def make_shell_context():
     return dict(app=app, db=db, Role=Role, User=User)
 
 
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
-
-    def __repr__(self):
-        return '<Role %r>' % self.name
 
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    #name = None
-    form = NameForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        user = User.query.filter_by(username=name).first()
-        if user is None:
-            user = User(username=name)
-            db.session.add(user)
-            session['known'] = False
-            if app.config['MAIL_ADMIN']:
-                send_email(app.config['MAIL_ADMIN'], 'New user <%s> register' % name, 'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = name
-        form.name.data = ''
-        return redirect(url_for('index'))
-    return render_template('index.html', name=session.get('name'), known=session.get('known', False), form=form)
-
-@app.route('/user/<username>')
-def user(username):
-    return render_template('user.html', username=username)
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
---
+++ config.py
@@ -0,0 +1,51 @@
+import os
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.yeah.net'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', 1]
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SUBJECT_PREFFIX = '[cmLighters\' Blog]'
+    MAIL_SENDER = 'Blog mail sender <mijechen@yeah.net>'
+    MAIL_ADMIN = os.environ.get('MAIL_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or \
+                                             'mysql+pymysql://{username}:{password}@{hostname}/{database}'.format(
+                                                 username='testuser', password='asdfjkl;', hostname='127.0.0.1',
+                                                 database='cm_flask_blog'
+                                             )
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI') or \
+                                             'mysql+pymysql://{username}:{password}@{hostname}/{database}'.format(
+                                                 username='testuser', password='asdfjkl;', hostname='127.0.0.1',
+                                                 database='cm_flask_blog'
+                                             )
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+
+
+config = dict(
+    development = DevelopmentConfig,
+    testing = TestingConfig,
+    production = ProductionConfig,
+
+    default = DevelopmentConfig
+)
+
