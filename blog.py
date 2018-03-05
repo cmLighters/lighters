af65f606d452026fedaf9cbcc874e0a5b5462193
cmLighters <mijechen@yeah.net>
2018-03-05 09:03:26 +0800

    redirect and use session

--- blog.py
+++ blog.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template
+#from datetime import datetime
+from flask import Flask, render_template, redirect, url_for, session
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Can u guess it.'
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    #name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', name=session.get('name'), form=form)
 
 @app.route('/user/<username>')
 def user(username):
