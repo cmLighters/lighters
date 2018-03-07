5367bd2ca4da4dd186121133ec4d67809fb63785
cmLighters <mijechen@yeah.net>
2018-03-07 21:13:32 +0800

    pagination

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import session, redirect, url_for, render_template, current_app, flash
+from flask import session, redirect, url_for, render_template, current_app, flash, request
 from flask_login import current_user, login_required
 
 from . import main
@@ -17,15 +17,23 @@ def index():
         db.session.add(post)
         flash('The post you write has been committed.')
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.created_at.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.created_at.desc()).paginate(
+        page, per_page=current_app.config['POSTS_PER_PAGE'], error_out=False
+    )
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts, pagination=pagination)
 
 
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = user.posts.order_by(Post.created_at.desc()).all()
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = user.posts.order_by(Post.created_at.desc()).paginate(
+        page, per_page=current_app.config['POSTS_PER_PAGE'], error_out=False
+    )
+    posts = pagination.items
+    return render_template('user.html', user=user, posts=posts, pagination=pagination)
 
 
 @main.route('/edit_profile', methods=['GET', 'POST'])
--- config.py
+++ config.py
@@ -13,6 +13,7 @@ class Config(object):
     MAIL_SUBJECT_PREFFIX = '[cmLighters\' Blog]'
     MAIL_SENDER = 'Blog mail sender <mijechen@yeah.net>'
     BLOG_ADMIN = os.environ.get('BLOG_ADMIN')
+    POSTS_PER_PAGE = 10
 
     @staticmethod
     def init_app(app):
