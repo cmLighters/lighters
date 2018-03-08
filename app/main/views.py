072afaccd540d8d43bf96a1d0e5b1f70aa92f75c
cmLighters <mijechen@yeah.net>
2018-03-08 20:04:15 +0800

    all posts or followd users posts in index

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import session, redirect, url_for, render_template, current_app, flash, request, abort
+from flask import session, redirect, url_for, render_template, current_app, flash, request, abort, make_response
 from flask_login import current_user, login_required
 
 from . import main
@@ -19,11 +19,18 @@ def index():
         flash('The post you write has been committed.')
         return redirect(url_for('.index'))
     page = request.args.get('page', 1, type=int)
-    pagination = Post.query.order_by(Post.created_at.desc()).paginate(
+    show_followed = False
+    if current_user.is_authenticated:
+        show_followed = bool(request.cookies.get('show_followed', ''))
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
+    pagination = query.order_by(Post.created_at.desc()).paginate(
         page, per_page=current_app.config['POSTS_PER_PAGE'], error_out=False
     )
     posts = pagination.items
-    return render_template('index.html', form=form, posts=posts, pagination=pagination)
+    return render_template('index.html', form=form, posts=posts, show_followed=show_followed, pagination=pagination)
 
 
 @main.route('/user/<username>')
@@ -171,3 +178,19 @@ def followed_by(username):
     follows = [ { 'user': item.followed, 'follow_time': item.follow_time} for item in pagination.items ]
     return render_template('followers.html', user=user, title='Followed of',
                            endpoint='.followed_by', follows=follows, pagination=pagination)
+
+
+@main.route('/all')
+@login_required
+def show_all():
+    response = make_response(redirect(url_for('.index')))
+    response.set_cookie('show_followed', '', max_age=30*24*60*60)
+    return response
+
+
+@main.route('/followed')
+@login_required
+def show_followed():
+    response = make_response(redirect(url_for('.index')))
+    response.set_cookie('show_followed', '1', max_age=30*24*60*60)
+    return response
